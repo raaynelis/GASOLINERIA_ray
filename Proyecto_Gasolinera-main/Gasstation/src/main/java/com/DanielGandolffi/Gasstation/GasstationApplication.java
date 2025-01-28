32490cf84a7b098b83b208007890e02d3a9c248d
raaynelis <raaynelishernandez2004@gmail.com>
2025-01-28 16:21:41 -0400

    Add files via upload

---
+++ Proyecto_Gasolinera-main/Gasstation/src/main/java/com/DanielGandolffi/Gasstation/GasstationApplication.java
@@ -0,0 +1,13 @@
+package com.DanielGandolffi.Gasstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasstationApplication.class, args);
+	}
+
+}
